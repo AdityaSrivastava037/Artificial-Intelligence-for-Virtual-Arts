0c3d56ebcf6a27b5eb5265bdd112094e9a6fbb04
kartikeygupta001 <109901714+kartikeygupta001@users.noreply.github.com>
2023-03-27 21:23:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from email.mime import image
+#from email.mime import image
 import HandTrackingModule as htm
 import cv2
 import numpy as np
@@ -14,6 +14,9 @@ for i in list:
     image = cv2.imread(f'{folder}/{i}')
     overlayList.append(image)
 header = overlayList[0]
+xp,yp = 0,0
+eraserThickness = 50
+shape = 'freestyle'
 detector = htm.handDetector(detectionCon=0.85,maxHands=1)
 imgcanvas = np.zeros((720,1280,3),np.uint8)
 
@@ -28,6 +31,8 @@ while True:
     if len(pos)!=0:
      xi,yi = pos[8][1:]
      xm, ym = pos[12][1:]
+     xt,yt = pos[4][1:]
+     dist = int((((yt-y1)**2)+((xt-x1)**2))**0.5)
      up = detector.fingersUp()
      if up[1] and up[2]:
          if y1 < 120:
@@ -83,18 +88,45 @@ while True:
              elif 1050 < x1 < 1200 and drawColor == (0, 255, 0):
                  header = overlayList[4]
                  shape = 'elipse'
+                cv2.circle(img,(x1,y1),25,drawColor,cv2.FILLED)
          if up[1] and up[2] == False:
             #print("Drawing Node")
             if xp==0 and yp==0:
                 xp,yp = x1,y1
-            cv2.linie(img,(xp,yp),(x1,y1),drawColor,10)
-            cv2.linie(imgcanvas,(xp,yp),(x1,y1),drawColor,10)
+                if drawColor== (0,0,0):
+                    if up[1] and up[4]:
+                        eraserThickness = dist
+                        #cv2.line(imgcanvas,(xp,yp),(x1,y1),drawColor,eraserThickness)
+                        cv2.circle(img,(x1,y1),eraserThickness,drawColor,cv2.FILLED)
+                        cv2.circle(imgcanvas,(x1,y1), eraserThickness, drawColor, cv2.FILLED)
+                    #cv2.line(imgcanvas,(xp, yp),(x1, y1),drawColor,50)
+                    else:
+                        if shape == 'freestyle':
+                            if dist<30:
+                            cv2.line(imgcanvas, (xp, yp), (x1, y1), drawColor, 10)
+                            #circle
+                            if shape == 'circle':
+                               
+                                #print(dist)
+                                cv2.circle(img,(x1,y1),dist,drawColor)
+                                
+                                #rectangle
+                                if shape == 'rectangle':
+                                    cv2.rectangle(img,(xt,yt),(x1,y1),drawColor)
+                                    if up[4]:
+                                        cv2.rectangle(imgcanvas, (xt, yt), (x1, y1), drawColor)
+                            
+            #cv2.linie(img,(xp,yp),(x1,y1),drawColor,10)
+            #cv2.linie(imgcanvas,(xp,yp),(x1,y1),drawColor,10)
             xp,yp = x1,y1
 
     img[0:210,0:1280]=header
 
     img_gray = cv2.cvtColor(imgcanvas,cv2.COLOUR_BGR2GRAY)
     _,imginv = cv2.threshold(img_gray,50,255,cv2.THRESH_BINARY_INV)
+     imginv = cv2.cvtColor(imginv, cv2.COLOR_GRAY2BGR)
+        img = cv2.bitwise_and(img, imginv)
+        img = cv2.bitwise_or(img, imgcanvas)
     imginv = cv2.cvtColor(imginv,cv2.color)
     cv2.imshow("video",img)
     cv2.imshow("Canvas",imgcanvas)
